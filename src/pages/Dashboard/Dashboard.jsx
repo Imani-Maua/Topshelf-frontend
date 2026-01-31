23095babd5b9a035ef2bc59bcf72fec014b8a234
Imani-Maua <imanimaua@gmail.com>
2026-01-31 14:27:28 +0100

    feat: add Dashboard and Participants pages

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard View</h1>
+      <p>This is where your stats will go later!</p>
+    </div>
+  );
+}
+
+export default Dashboard;
